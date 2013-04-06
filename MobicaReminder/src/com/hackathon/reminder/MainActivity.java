2b0b1dc0a63e3f7c50a818b0546f5e2d739836f8
Tomasz Frontczak <ta.frontczak@gmail.com>
2013-04-06 15:32:24 +0200

    Added Notifications and AlertReceiver

--- MobicaReminder/src/com/hackathon/reminder/MainActivity.java
+++ MobicaReminder/src/com/hackathon/reminder/MainActivity.java
@@ -1,31 +1,34 @@
 package com.hackathon.reminder;
 
-import java.util.Calendar;
+import com.hackathon.reminder.notifications.AbstractNotification;
+import com.hackathon.reminder.notifications.MisNotification;
 
-import com.hackathon.reminder.R;
-import android.os.Bundle;
-import android.app.Activity;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import roboguice.inject.InjectView;
 import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import roboguice.activity.RoboActivity;
-import roboguice.inject.ContentView;
 
 @ContentView(R.layout.activity_main)
 public class MainActivity extends RoboActivity implements OnClickListener {
 
+	@InjectView(R.id.B1)
+	Button mB1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		((Button)findViewById(R.id.B1)).setOnClickListener(this);
+		((Button)findViewById(R.id.B2)).setOnClickListener(this);
 	}
 
 	@Override
@@ -41,36 +44,20 @@ public class MainActivity extends RoboActivity implements OnClickListener {
 			registerAlarm();
 			break;
 		case R.id.B2:
-			notif();
+			
 			break;
 		}
 	}
 
 	private void registerAlarm(){
+		AbstractNotification notification = new MisNotification();
 		Intent intent = new Intent(this, AlarmReceiver.class);
 		intent.setAction(AlarmReceiver.ALARM_RECIVE_ACTION);
+		intent.putExtra(AlarmReceiver.SERIALIZE_ID, notification);
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTimeInMillis(System.currentTimeMillis());
 		AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 		alarm.cancel(pendingIntent);
-		alarm.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES, pendingIntent);
-	}
-	
-	private void notif(){
-		/*Notification.Builder mBuilder = new NotificationCompat.Builder(this)
-		    .setSmallIcon(R.drawable.ic_launcher)
-		    .setContentTitle("notification")
-		    .setContentText("HACKATHON!");
-		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		notificationManager.notify(666, mBuilder.build());*/
-		/*NotificationManager mNM = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-		Notification notification = new Notification(R.drawable.ic_launcher, "Name", System.currentTimeMillis());
-		notification.flags |= Notification.FLAG_AUTO_CANCEL;
-		Intent intent = getProperIntent(true, p);
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 666, intent, 0);
-		notification.setLatestEventInfo(this, "Name 2", contentIntent);
-		mNM.notify(666, notification);*/
-	}
+		alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES, pendingIntent);
+		Log.e("register", "YES");
 	}
 }
---
+++ MobicaReminder/src/com/hackathon/reminder/notifications/AbstractNotification.java
@@ -0,0 +1,16 @@
+package com.hackathon.reminder.notifications;
+
+import java.io.Serializable;
+
+import com.hackathon.reminder.R;
+
+public abstract class AbstractNotification implements Serializable {
+
+	public abstract String getURL();
+	public abstract String getTitle();
+	public abstract String getText();
+	
+	public int getRid(){
+		return R.drawable.ic_launcher;
+	}
+}
